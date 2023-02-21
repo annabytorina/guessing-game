24703aa0d2372f6cebe2ff1ea83224ec04bf4637
Ania <hannabutorina@gmail.com>
2023-02-21 17:34:21 +0300

    feat: add solution need fix

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,38 @@
 class GuessingGame {
-    constructor() {}
+    constructor(number) {
+        this.number = number;
+    }
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        if (this.min <= this.max) {
+            this.mid = Math.floor((this.min + this.max)/2);
+            if (this.mid === this.number) {
+                return this.mid;
+            } else {
+                if (this.mid > this.number) {
+                    return this.greater();
+                } else {
+                    return this.lower();
+                }
+            }
+        } else {
+            return -1;
+        }
     }
 
     lower() {
-
+        this.min = this.mid + 1;
+        return this.guess();
     }
 
     greater() {
-
+        this.max = this.mid - 1;
+        return this.guess();
     }
 }
 
